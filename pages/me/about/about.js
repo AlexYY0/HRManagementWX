84e97b255d3479df99c0902702b4fe645cd05c8c
alexyy0 <1970912595@qq.com>
2020-03-16 21:34:09 +0800

    Initial Commit

---
+++ pages/me/about/about.js
@@ -0,0 +1,16 @@
+// pages/me/about/about.js
+//获取应用实例
+var app = getApp();
+Page({
+  data: {
+    showLog: false
+  },
+  onLoad: function () {
+
+  },
+  toggleLog: function () {
+    this.setData({
+      showLog: !this.data.showLog
+    });
+  }
+});
